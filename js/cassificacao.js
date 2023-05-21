06e71a24ab03425f485152025976ec6b87dba9e3
Kaique Geraldo <kaiique2404@gmail.com>
2023-05-21 14:14:17 -0300

    começo modo noturno

--- js/cassificacao.js
+++ js/cassificacao.js
@@ -1,29 +1,50 @@
 // barraHamburger
 function toggleBar() {
-    let toggleBar = document.querySelector(".navList")
-    let menuMobile = document.querySelector(".menuMobile")
-    let x = document.getElementById("x")
+    let toggleBar = document.querySelector(".navList");
+    let menuMobile = document.querySelector(".menuMobile");
+    let x = document.getElementById("x");
+    let pesquisa = document.querySelector(".pesquisa");
 
     if (toggleBar.className === "navList") {
         toggleBar.classList.add("active");
-        x.style.display = "block"
+        toggleBar.style.zIndex = "1";
+        x.style.display = "block";
         document.body.style.overflow = "hidden";
         menuMobile.style.opacity = "0";
+        pesquisa.style.zIndex = "0";
     }
     else {
         toggleBar.classList.remove("active");
+        toggleBar.style.zIndex = "4";
         x.style.display = "inherit"
         document.body.style.overflow = "inherit";
         menuMobile.style.opacity = "1";
+        pesquisa.style.zIndex = "5";
     }
 }
 // fim barraHambuerger
 
 // btn modoNoturno
-let btn = document.querySelector("#btn")
+let btn = document.querySelector("#btn");
+let body = document.querySelector("body");
+let navMenu = document.querySelector(".navMenu");
+let menuMobile = document.querySelector(".menuMobileNoturno");
+let user = document.querySelector(".userNoturno");
+let pesquisa = document.querySelector(".pesquisaNoturno");
+let x = document.querySelector(".xNoturno")
+let textTittle = document.querySelector(".tittle");
+let textSub = document.querySelector(".sub");
 
 btn.addEventListener("click", () =>{
     btn.classList.toggle("activeButton");
+    body.classList.toggle("meActive");
+    navMenu.classList.toggle("meActive");
+    menuMobile.classList.toggle("meActive");
+    user.classList.toggle("meActive");
+    pesquisa.classList.toggle("meActive");
+    x.classList.toggle("meActive");
+    textTittle.classList.toggle("meActive");
+    textSub.classList.toggle("meActive");
 })
 // fim btn modo Noturno
 
